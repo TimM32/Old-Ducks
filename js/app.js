36b0898444e97c0c646b21ebf5efef66007eb651
TimM32 <timmaupin@live.com>
2023-03-28 11:56:59 -0500

    make the interface and css cleaner

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('old ducks babay')
+console.log('old ducks babay');
 
 let productContainer = document.getElementById('product_container');
 let resultsButton = document.getElementById('results');
@@ -14,85 +14,85 @@ Products.allProductsArray = [];
 
 
 function Products(name, src) {
-    this.name = name;
-    this.imageSrc = src;
-    this.views = 0;
-    this.clickedOn = 0;
-    Products.allProductsArray.push(this);
+  this.name = name;
+  this.imageSrc = src;
+  this.views = 0;
+  this.clickedOn = 0;
+  Products.allProductsArray.push(this);
 }
 console.log('duccckksss', Products.allProductsArray);
 
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * Products.allProductsArray.length);
+  return Math.floor(Math.random() * Products.allProductsArray.length);
 }
 
 function renderProducts() {
-    let product1 = getRandomNumber();
-    let product2 = getRandomNumber();
-    let product3 = getRandomNumber();
-    console.log(typeof product1, product2, product3);
+  let product1 = getRandomNumber();
+  let product2 = getRandomNumber();
+  let product3 = getRandomNumber();
+  console.log(typeof product1, product2, product3);
 
-    while (product1 === product2 || product1 === product3) {
-        product1 = getRandomNumber();
-    }
+  while (product1 === product2 || product1 === product3) {
+    product1 = getRandomNumber();
+  }
 
-       
-    while (product2 === product1 || product2 === product3) {
-            product2 = getRandomNumber();
-        }
-    
 
-    image1.src = Products.allProductsArray[product1].imageSrc;
-    image2.src = Products.allProductsArray[product2].imageSrc;
-    image3.src = Products.allProductsArray[product3].imageSrc;
+  while (product2 === product1 || product2 === product3) {
+    product2 = getRandomNumber();
+  }
 
-    image1.alt = Products.allProductsArray[product1].name;
-    image2.alt = Products.allProductsArray[product2].name;
-    image3.alt = Products.allProductsArray[product3].name;
 
-    console.log(image1, image2, image3);
+  image1.src = Products.allProductsArray[product1].imageSrc;
+  image2.src = Products.allProductsArray[product2].imageSrc;
+  image3.src = Products.allProductsArray[product3].imageSrc;
 
-    Products.allProductsArray[product1].views++;
-    Products.allProductsArray[product2].views++;
-    Products.allProductsArray[product3].views++;
+  image1.alt = Products.allProductsArray[product1].name;
+  image2.alt = Products.allProductsArray[product2].name;
+  image3.alt = Products.allProductsArray[product3].name;
+
+  console.log(image1, image2, image3);
+
+  Products.allProductsArray[product1].views++;
+  Products.allProductsArray[product2].views++;
+  Products.allProductsArray[product3].views++;
 }
 
 function handleProductClick(event) {
-    console.log(event.target);
-    if (event.target === productContainer) {
-        alert('Please click product image!');
-    }
+  console.log(event.target);
+  if (event.target === productContainer) {
+    alert('Please click product image!');
+  }
 
 
-clicks++;
-let clickedOnProduct = event.target.alt;
-console.log('🚀 ~ file: app.js:68 ~ clickedOnProduct:', clickedOnProduct);
-for (let i = 0; i < Products.allProductsArray.length; i++) {
+  clicks++;
+  let clickedOnProduct = event.target.alt;
+  console.log('🚀 ~ file: app.js:68 ~ clickedOnProduct:', clickedOnProduct);
+  for (let i = 0; i < Products.allProductsArray.length; i++) {
     if (clickedOnProduct === Products.allProductsArray[i].name) {
-        Products.allProductsArray[i].clickedOn++;
-        break;
+      Products.allProductsArray[i].clickedOn++;
+      break;
     }
-}
+  }
 
 
-if(clicks === maxClicks) {
+  if(clicks === maxClicks) {
     productContainer.removeEventListener('click', handleProductClick);
     resultsButton.addEventListener('click', renderResults);
-    productContainer.className = 'no-voting'
-} else {
-    console.log('products are renderinggg')
+    productContainer.className = 'no-voting';
+  } else {
+    console.log('products are renderinggg');
     renderProducts();
-}
+  }
 }
 
 function renderResults() {
-    let ul = document.getElementById('results_list');
-    for(let i = 0; i , Products.allProductsArray.length; i++) {
-        let li = document.createElement('li');
-        li.textContent = `${Products.allProductsArray[i].name} had ${Products.allProductsArray[i].views} views and was clicked ${Products.allProductsArray[i].clickedOn} times.`;
-        ul.appendChild(li);
-    }
+  let ul = document.getElementById('results_list');
+  for(let i = 0; i , Products.allProductsArray.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${Products.allProductsArray[i].name} had ${Products.allProductsArray[i].views} views and was clicked ${Products.allProductsArray[i].clickedOn} times.`;
+    ul.appendChild(li);
+  }
 }
 
 new Products ('Roller bag', '../images/bag.jpg');
@@ -106,7 +106,7 @@ new Products ('Cthulhu', '../images/cthulhu.jpg');
 new Products ('Dog', '../images/dog-duck.jpg');
 new Products ('Dragon', '../images/dragon.jpg');
 new Products ('Pen', '../images/pen.jpg');
-new Products ('Pet Sweep', '../images/pet-sweep.jpg')
+new Products ('Pet Sweep', '../images/pet-sweep.jpg');
 new Products ('Scissors', '../images/scissors.jpg');
 new Products ('Shark', '../images/shark.jpg');
 new Products ('Sweep', '../images/sweep.png');
@@ -118,4 +118,4 @@ new Products ('Wine Glass', '../images/wine-glass.jpg');
 
 renderProducts();
 
-productContainer.addEventListener('click', handleProductClick)
+productContainer.addEventListener('click', handleProductClick);
